36f27853ba912af32722f81efdd572ab99010890
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-05-31 00:14:28 +0900

    関連間違ってたので修正

--- app/models/canonical_route_instruments_for_recruit.rb
+++ app/models/canonical_route_instruments_for_recruit.rb
@@ -1,5 +1,5 @@
 class CanonicalRouteInstrumentsForRecruit < ApplicationRecord
-  has_one :canonical_routes, :as => :canonical_routable
+  has_one :canonical_route, :as => :canonical_routable
 
   has_many :canonical_route_instruments_for_recruits_relations
   has_many :instruments, :through => :canonical_route_instruments_for_recruits_relations
