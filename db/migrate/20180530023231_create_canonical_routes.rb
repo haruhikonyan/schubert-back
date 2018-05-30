6f2881da659df8668f7eafdcfaba4f75938c9c74
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-05-31 00:07:06 +0900

    indexの名前変更

--- db/migrate/20180530023231_create_canonical_routes.rb
+++ db/migrate/20180530023231_create_canonical_routes.rb
@@ -1,7 +1,7 @@
 class CreateCanonicalRoutes < ActiveRecord::Migration[5.1]
   def change
     create_table :canonical_routes do |t|
-      t.references :canonical_routable, polymorphic: true
+      t.references :canonical_routable, polymorphic: true, index: {name: "index_canonical_routes"}
       t.string :path
       t.string :label
       t.string :category
--- db/migrate/20180530024556_create_canonical_route_instruments_for_recruits_relations.rb
+++ db/migrate/20180530024556_create_canonical_route_instruments_for_recruits_relations.rb
@@ -1,8 +1,8 @@
 class CreateCanonicalRouteInstrumentsForRecruitsRelations < ActiveRecord::Migration[5.1]
   def change
     create_table :canonical_route_instruments_for_recruits_relations do |t|
-      t.belongs_to :canonical_route_instruments_for_recruit, index: true, null: false
-      t.belongs_to :instrument, index: true, null: false
+      t.belongs_to :canonical_route_instruments_for_recruit, index: {name: "index_crifrr_on_canonical_route_instruments_for_recruit_id"}, null: false
+      t.belongs_to :instrument, index: {name: "index_crifrr_on_instrument_id"}, null: false
 
       t.timestamps
     end
