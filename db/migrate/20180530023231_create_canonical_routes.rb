9172e1c9853de22abd1e0f65d0b517328bced347
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-06-07 23:17:56 +0900

    migrateファイル修正

--- db/migrate/20180530023231_create_canonical_routes.rb
+++ db/migrate/20180530023231_create_canonical_routes.rb
@@ -2,12 +2,12 @@ class CreateCanonicalRoutes < ActiveRecord::Migration[5.1]
   def change
     create_table :canonical_routes do |t|
       t.references :canonical_routable, polymorphic: true, index: {name: "index_canonical_routes"}
-      t.string :path
-      t.string :label
-      t.string :category
+      t.string :path, null: false
+      t.string :label, null: false
+      t.string :category, null: false
       t.integer :order_in_Category
-      t.boolean :is_listed_on_top
-      t.boolean :is_listed_on_model_top      
+      t.boolean :is_listed_on_top, default: false
+      t.boolean :is_listed_on_model_top, default: false     
       t.timestamps
     end
   end
